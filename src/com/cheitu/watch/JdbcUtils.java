9cb936639706a0c655238283c1ad4101f5e555bf
Cheitu <c88@outlook.com>
2015-11-15 21:57:29 +0800

    ‘’

--- src/com/cheitu/watch/JdbcUtils.java
+++ src/com/cheitu/watch/JdbcUtils.java
@@ -22,7 +22,7 @@ public final class JdbcUtils {
      * 获取连接
      */
     public static Connection getConnection() throws SQLException{
-    	String url="jdbc:mysql://47.88.23.227:3306/sniffer?useUnicode=true&amp;characterEncoding=utf-8";
+    	String url="jdbc:mysql://****:3306/sniffer?useUnicode=true&amp;characterEncoding=utf-8";
 		String user="root";
 		String password="123456";	
     	 
